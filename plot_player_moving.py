2b438c3d378a2014fad456dee254550b249ebee6
acercyc <acercyc@gmail.com>
2017-01-19 17:35:27 +0900

    add plot moving

--- plot_player_moving.py
+++ plot_player_moving.py
@@ -1,10 +1,7 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import time
-import lib.lib as lib
+# 1.0 - Acer 2017/01/19 17:25
+
+import football.lib as fb
 import pandas as pd
-import sys
 
-d = pd.read_csv('valid_data/2016052108_player.csv')
-lib.plot_moving(d)
-
+d = pd.read_csv('valid_data/2016022707_player.csv')
+fb.plot_moving_playerBased(d)
